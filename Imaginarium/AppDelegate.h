9e19ca3209148a0e929e8515bb843d05c62ac120
Igor Moiseyenko <imoiseyenko93@gmail.com>
2015-03-19 11:44:59 +0300

    Initial Commit

---
+++ Imaginarium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by Igor on 3/19/15.
+//  Copyright (c) 2015 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Imaginarium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imaginarium
+//
+//  Created by Igor on 3/19/15.
+//  Copyright (c) 2015 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
